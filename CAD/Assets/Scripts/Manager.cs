dfb23838bdec1cf8ad538f360801ab2d9990f476
EduardoCordeiro <eduardoalcordeiro@gmail.com>
2018-01-30 14:34:43 +0100

    Initial Commit -> Added CAD files

---
+++ CAD/Assets/Scripts/Manager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    #region Statics
+    /// <summary>
+    /// Project Manager instance
+    /// </summary>
+    public static Manager instance { get; private set; }
+    #endregion
+
+
+    // Use this for initialization
+    public void Start() {
+
+        // Initialize the singleton instance
+        Manager.instance = this;
+
+        Debug.Log("Hand Manager - SceneCreate()");
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ CAD/Assets/Scripts/ObjectExplosion.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectExplosion : MonoBehaviour {
+
+    private List<Dictionary<string, GameObject>> partLists;
+
+    private List<Transform> initialPositions;
+
+    public int depthLevel = 0;
+
+	// Use this for initialization
+	void Start () {
+
+        partLists = new List<Dictionary<string, GameObject>>();
+
+        partLists.Add(CreatePartList(depthLevel));
+
+        initialPositions = new List<Transform>();
+
+        StartingPositions(partLists[0]);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void StartingPositions(Dictionary<string, GameObject> partList) {
+
+        foreach(GameObject part in partList.Values)
+            initialPositions.Add(part.transform);
+    }
+
+    Dictionary<string, GameObject> CreatePartList(int depthLevel) {
+
+        Dictionary<string, GameObject>  partList = new Dictionary<string, GameObject>();
+
+        for(int i = 0; i < transform.childCount; i++) {
+
+            Transform child = transform.GetChild(i);
+
+            partList.Add(child.name + i, child.gameObject);
+
+            // Adding Grand children if Depth Level > 0
+            if(depthLevel > 0 && child.childCount > 1)
+                for(int j = 0; j < child.childCount; j++)
+                    partList.Add(child.GetChild(j).name + i.ToString() + j.ToString(), child.GetChild(j).gameObject);
+        }
+
+        return partList;
+    }
+
+    void createSphere() {
+
+        Vector3 center = this.transform.position;
+
+        float radius = 5.0f;
+    }
+}
