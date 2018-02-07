d2db671dc80ed8b322934dcc037197ad2e7c9c07
EduardoCordeiro <eduardoalcordeiro@gmail.com>
2018-02-07 13:25:42 +0100

    Added the new models from Katia, dont understand the structure.

--- CAD/Assets/Scripts/Utility/HierarchyCreator.cs
+++ CAD/Assets/Scripts/Utility/HierarchyCreator.cs
@@ -6,11 +6,8 @@ namespace CAD.Utility {
 
     public class HierarchyCreator : MonoBehaviour {
 
-        public List<GameObject> partList;
-
         // Use this for initialization
         void Start() {
-
         }
 
         // Update is called once per frame
@@ -20,9 +17,24 @@ namespace CAD.Utility {
 
         public void CreateHierarchy() {
 
-            foreach(GameObject part in partList) {
+        }
 
-                part.transform.parent = this.transform;
+        public void ReadHierarchy() {
+
+            // Remenber we ignore the deepest level (due to mesh creation and disasembly)
+            int depthLevel = 0;
+
+            if(transform.childCount > 0) {
+
+                depthLevel++;
+
+                List<GameObject> currentLevel = new List<GameObject>();
+
+                for(int i = 0; i < transform.childCount; i++) {
+
+                    currentLevel.Add(transform.GetChild(i).gameObject);
+                }
+
             }
         }
     }
