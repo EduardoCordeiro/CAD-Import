b43ac9d71e04caa2562ac6c40251e6b839f688da
EduardoCordeiro <eduardoalcordeiro@gmail.com>
2018-02-15 16:57:46 +0100

    Deleted CaracteristicsList, now only Caracteristics exists.

---
+++ CAD/Assets/Scripts/Caracteristic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CAD.Utility {
+
+    [System.Serializable]
+    public class Caracteristic {
+
+        public int ID;
+
+        public string labels;
+
+        public float mushape;
+
+        public float muPos;
+
+        public float mujoint;
+
+        public float mustruct;
+
+        public float localMeasure;
+
+        public float partialMeasure;
+
+        public float globalMeasure;
+    }
+}
