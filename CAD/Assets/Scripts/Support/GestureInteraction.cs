474f9e6e569d87a4d8526f3e1ee463709f7846bc
EduardoCordeiro <eduardoalcordeiro@gmail.com>
2018-03-01 17:07:19 +0100

    Added Dome. First steps on the gestures, pinching.

--- CAD/Assets/Scripts/Support/GestureInteraction.cs
+++ CAD/Assets/Scripts/Support/GestureInteraction.cs
@@ -2,23 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System;
+
 using Leap;
+using Leap.Unity.Interaction;
 
 namespace CAD.Support {
 
-    public class GestureListener  {
+    public class GestureInteraction : MonoBehaviour {
 
-        private Controller controller;
+        public InteractionBehaviour interactionBehaviour;
+        List<InteractionBehaviour> behavioursList;
 
         // Use this for initialization
         void Start() {
 
-            controller = new Controller();
+            behavioursList = new List<InteractionBehaviour>();
+
+            foreach(Transform child in transform)
+                behavioursList.Add(child.GetComponent<InteractionBehaviour>());
         }
 
         // Update is called once per frame
         void Update() {
 
+        }
+
+        public void ClosedFist() {
+
+            Debug.Log("ClosedFist()");
+        }
+
+        public void OpenHand() {
+
+            Debug.Log("OpenHand");
+        }
+
+        public void PointingFinger() {
+
+            print("PointingFinger()");
+        }
+
+        public void NotPointing() {
+
+            print("NotPointing()");
+        }
+
+
+        // Grasping
+        public void GraspPart() {
+
+            print("I began a grasp");
+
+
+        }
+
+        public void StopGrasp() {
+
+            print("Stopped grasp");
         }
     }
 }
