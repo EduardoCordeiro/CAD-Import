2c9f93d28798a8942c9ca1b26c0436bb86a98939
EduardoCordeiro <eduardoalcordeiro@gmail.com>
2018-03-01 11:10:42 +0100

    FINAL MERGE; its finnaly working now, the scene had merge conflicts.

--- CAD/Assets/Scripts/MeasureFirstScene/MeasureSelection.cs
+++ CAD/Assets/Scripts/MeasureFirstScene/MeasureSelection.cs
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Commiting more meta files
-=======
->>>>>>> tentative
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -45,51 +38,3 @@ public class MeasureSelection : MonoBehaviour {
         SceneManager.LoadScene("Referencial");
     }
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class MeasureSelection : MonoBehaviour {
-
-
-    private void OnTriggerEnter(Collider collider)
-    {
-        print("Enter in the trigger");
-
-        var sphereSelectedName = this.gameObject.name;
-
-        switch (sphereSelectedName)
-        {
-            case "LocalSphere":
-                MeasureInformation.measureType = MeasureInformation.MeasureType.Local;
-                break;
-            case "PartialSphere":
-                MeasureInformation.measureType = MeasureInformation.MeasureType.Partial;
-                break;
-            case "GlobalSphere":
-                MeasureInformation.measureType = MeasureInformation.MeasureType.Global;
-                break;
-        }
-
-    }
-
-    private void OnTriggerStay(Collider collider)
-    {
-
-    }
-
-
-    private void OnTriggerExit(Collider collider)
-    {
-        SceneManager.LoadScene("Referencial");
-    }
-}
->>>>>>> 16b4b237dad37d5af7d6d5976c001affb80ba436
-=======
->>>>>>> Commiting more meta files
-=======
->>>>>>> tentative
